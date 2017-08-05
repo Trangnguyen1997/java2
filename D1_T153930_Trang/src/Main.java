5eedb3c334bd4109d6c98d466929802b0095d22b
US <US@A1M33>
2017-08-05 14:20:53 +0700

    thuc hien chuc nang mo cua so moi

---
+++ D1_T153930_Trang/src/Main.java
@@ -0,0 +1,79 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class Main extends JFrame {
+
+	private JDesktopPane contentPane;
+
+
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("File");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNew = new JMenuItem("New");
+		mnNewMenu.add(mntmNew);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mnNewMenu.add(mntmOpen);
+		
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mnNewMenu.add(mntmSave);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnNewMenu.add(mntmExit);
+		
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
+		
+		JMenu mnTool = new JMenu("Tool");
+		menuBar.add(mnTool);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		contentPane = new JDesktopPane();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		// Xu y su kien
+		mntmNew.addActionListener(new ActionListener() {
+			public void actionerFormed(ActionListener args){
+				JInternalFrame newWindow = new JInternalFrame("New Window 1", true, true , true);
+						newWindow.setVisible(true);
+						newWindow.setSize(300, 300);
+						contentPane.add(newWindow);
+			}
+			
+			
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+
+}
